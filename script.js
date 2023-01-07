4087560a6afe2bb202607c4540c2b635ba570eec
Zaide Kurti <zaiido1920@gmail.com>
2023-01-07 14:36:06 +0100

    Added M Animation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function headerChange() {
+const headerChange = function () {
   let headerNode = document.querySelector("header");
   let specialLinkNode = document.querySelector(
     "header .container nav .special-link"
@@ -10,6 +10,53 @@ function headerChange() {
   } else {
     headerNode.style.backgroundColor = "#FFC017";
   }
-}
+};
+
+// ----------------------- M Animation ----------------------- //
+
+let mNode = document.querySelectorAll(
+  "div.hero-m > svg > g > g > g[opacity='1']> g > g > path"
+);
+let randomNumbersArray = [];
+
+const generateRandomNumbers = function () {
+  for (let i = 0; i < 30; i++) {
+    let randomNumber = Math.floor(Math.random() * 131);
+    if (randomNumbersArray.includes(randomNumber)) {
+      i--;
+    } else {
+      randomNumbersArray.push(randomNumber);
+    }
+  }
+};
+
+const addM = function (mNode) {
+  let appearIndex = randomNumbersArray.length - 1;
+
+  const appearM = setInterval(() => {
+    if (appearIndex === 0) {
+      clearInterval(appearM);
+    }
+    let randomNumber = randomNumbersArray[appearIndex--];
+    mNode[randomNumber].classList.remove("disappear");
+  }, 100);
+};
+
+const animateM = function () {
+  let disappearIndex = 0;
+
+  const disappearM = setInterval(() => {
+    if (disappearIndex === randomNumbersArray.length - 1) {
+      clearInterval(disappearM);
+      addM(mNode);
+    }
+    let randomNumber = randomNumbersArray[disappearIndex++];
+    mNode[randomNumber].classList.add("disappear");
+  }, 100);
+};
+
+animateM();
+setInterval(animateM, 6000);
 
 window.onscroll = headerChange;
+window.onload = generateRandomNumbers;
